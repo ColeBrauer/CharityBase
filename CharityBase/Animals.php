f699476c902d5bf297b1b0ced498072a67b99d20
Cole Brauer <46873364+ColeBrauer@users.noreply.github.com>
2019-06-17 19:01:13 -0700

    Add files via upload
    Created shortlist section, no more access from reg user to edit animals

--- CharityBase/Animals.php
+++ CharityBase/Animals.php
@@ -20,24 +20,24 @@
 	<img class="picture" src=""/>
 		<div class="contenttext">
         	<h1>Animals</h1>
-            <p>This section is all about animals.</p>
-			<p>adding a new animal:</p>
-			<p>animal id&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; breed</p>
+			<p>Add animal to shortlist:</p>
+			<p>Animal id&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Organization ID</p>
 			<form method="POST" action="Animals.php">
-			<p><input type="text" name="aid" size="16"><input type="text" name="aName" size="16"><input type="text" name="aBreed" size="16"><input type="text" name="adoption status" size="16"><input type="text" name="age" size="16"><input type="text" name="description" size="16"><input type="text" name="gender" size="16"><input type="text" name="Personality" size="16"><input type="text" name="health" size="16"><input type="text" name="Adoptionfee" size="16">
-			<input type="submit" value="Add" name="addsubmit"></p>
+			<p><input type="text" name="Animal_ID" size="16"><input type="text" name="Organization_ID" size="16">
+			<input type="submit" value="Add" name="ShortlistAdd"></p>
 			</form>
-			<p>Deleting an animal:</p>
-			<p>animal id</p>
+			<p>Delete from Shortlist:</p>
+			<p>Animal id&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Organization ID</p>
 			<form method="POST" action="Animals.php">
-			<p><input type="text" name="AId" size="16">
-			<input type="submit" value="Delete" name="deletesubmit"></p>
+			<p><input type="text" name="Animal_ID" size="16"><input type="text" name="Organization_ID" size="16">
+			<input type="submit" value="Delete" name="ShortlistDelete"></p>
 			</form>
 			<form method="POST" action="Animals.php">
 			<input type="submit" value="reset" name="reset"></p>
 			</form>
 			<?php include 'db.php';
-				  run_table();?>
+				  run_Animaltable();
+				  run_Shortlist();?>
         </div>
     </div>
 </div>
@@ -78,48 +78,52 @@ if ($db_conn) {
 		executePlainSQL("Drop table animal");
 
 		echo "<br> creating new table <br>";
-		executePlainSQL("create table animal (aid number,
-		aName varchar2(30),
-		aBreed varchar2(30),
-		status varchar2(20),
-		age number,
-		description varchar2(100),
-		gender varchar2(7),
-		personality varchar2(50),
-		Health varchar2(50),
-		Adoptionfee number,
-		primary key (aid))");
+		executePlainSQL("create table animal (
+		Animal_ID CHAR(6), 
+		Organization_ID CHAR(6),
+		Name VARCHAR(20),
+		Age INTEGER,
+		Gender CHAR(1), 
+		Breed VARCHAR(20),
+		Personality VARCHAR(20), 
+		Health_Considerations VARCHAR(20), 
+		Intake_Date DATE,
+		Price REAL)");
+		OCICommit($db_conn);
+		
+		echo "<br> dropping table <br>";
+		executePlainSQL("Drop table Animal_Shortlist");
+
+		echo "<br> creating new table <br>";
+		executePlainSQL("create table Animal_Shortlist (
+		User_ID CHAR(6),
+		Animal_ID CHAR(6), 
+		Organization_ID CHAR(6)
+		)");
 		OCICommit($db_conn);
 
 	} else
-		if (array_key_exists('addsubmit', $_POST)) {
+		if (array_key_exists('ShortlistAdd', $_POST)) {
 		$tuple = array (
-			":bind1" => $_POST['aid'],
-			":bind2" => $_POST['aName'],
-			":bind3" => $_POST['aBreed'],
-			":bind4" => $_POST['status'],
-			":bind5" => $_POST['age'],
-			":bind6" => $_POST['description'],
-			":bind7" => $_POST['gender'],
-			":bind8" => $_POST['personality'],
-			":bind9" => $_POST['Health'],
-			":bind10" => $_POST['Adoptionfee'],
+			":bind1" => $_POST['Animal_ID'],
+			":bind2" => $_POST['Organization_ID']
 		);
 		$alltuples = array (
 			$tuple
 		);
-		executeBoundSQL("insert into animal values (:bind1, :bind2, :bind3, :bind4, :bind5, :bind6, :bind7, :bind8, :bind9, :bind10)", $alltuples);
+		executeBoundSQL("insert into Animal_Shortlist values (NULL,:bind1, :bind2)", $alltuples);
 		OCICommit($db_conn);
 
 		} else
-			if (array_key_exists('deletesubmit', $_POST)) {
+			if (array_key_exists('ShortlistDelete', $_POST)) {
 				$tuple = array (
-					":bind1" => $_POST['AId']
+					":bind1" => $_POST['Animal_ID'],
+					":bind2" => $_POST['Organization_ID']
 				);
 				$alltuples = array (
 					$tuple
 				);
-				executeBoundSQL("delete from animal where aid=:bind1", $alltuples);
+				executeBoundSQL("delete from Animal_Shortlist where Animal_ID==:bind1 AND Organization_ID==:bind2 ", $alltuples);
 				OCICommit($db_conn);
 	OCICommit($db_conn);
 	}
@@ -129,12 +133,23 @@ if ($db_conn) {
 	echo htmlentities($e['message']);
 }
 
-function run_table(){
+function run_Animaltable(){
 	if ($_POST && $success) {
 		header("location: Animals.php");
 	} else {
-		$result = executePlainSQL("select * from animal");
-           $columnNames = array("animal id", "animal name", "animal breed", "adptoption status", "age", "description", "gender", "Personality", "Health considerations", "Adoption fee");
+		$result = executePlainSQL("select * from Sheltered_Animal");
+           $columnNames = array("animal id", "Org ID", "name", "Age", "Gender", "Breed", "Personality", "Health", "Since", "Price");
+           printTable($result, $columnNames);
+	}
+	OCICommit($db_conn);
+}
+
+function run_Shortlist(){
+	if ($_POST && $success) {
+		header("location: Animals.php");
+	} else {
+		$result = executePlainSQL("select * from Animal_Shortlist");
+           $columnNames = array("UserID(YOU)", "animal id", "Org ID");
            printTable($result, $columnNames);
 	}
 	OCICommit($db_conn);
