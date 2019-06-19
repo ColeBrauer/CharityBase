2403a872db90e63f21765e96c19e3a8f7a7a1865
UBC Student <dreamle1@valdes.students.cs.ubc.ca>
2019-06-18 18:13:35 -0700

    small animal change

--- CharityBase/Animals.php
+++ CharityBase/Animals.php
@@ -21,23 +21,22 @@
 		<div class="contenttext">
         	<h1>Animals</h1>
 			<p>Add animal to shortlist:</p>
-			<p>Animal id&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Organization ID</p>
+			<p>UserID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Animal id&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Organization ID</p>
 			<form method="POST" action="Animals.php">
-			<p><input type="text" name="Animal_ID" size="16"><input type="text" name="Organization_ID" size="16">
+			<p><input type "text" name="userid" size="16"><input type="text" name="Animal_ID" size="16"><input type="text" name="Organization_ID" size="16">
 			<input type="submit" value="Add" name="ShortlistAdd"></p>
 			</form>
 			<p>Delete from Shortlist:</p>
-			<p>Animal id&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Organization ID</p>
+			<p>UserID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Animal id&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Organization ID</p>
 			<form method="POST" action="Animals.php">
-			<p><input type="text" name="Animal_ID" size="16"><input type="text" name="Organization_ID" size="16">
+			<p><input type "text" name="userid" size="16"><input type="text" name="Animal_ID" size="16"><input type="text" name="Organization_ID" size="16">
 			<input type="submit" value="Delete" name="ShortlistDelete"></p>
 			</form>
-			<form method="POST" action="Animals.php">
-			<input type="submit" value="reset" name="reset"></p>
-			</form>
+			<p>Animals available for adoption:</p>
 			<?php include 'db.php';
-				  run_Animaltable();
-				  run_Shortlist();?>
+				  run_Animaltable();?>
+			<p>Animals shortlist:</p>			
+			<?php run_Shortlist();?>
         </div>
     </div>
 </div>
@@ -69,61 +68,30 @@
 
 
 <?php
-
-
-include 'db.php';
 if ($db_conn) {
-	if (array_key_exists('reset', $_POST)) {
-		echo "<br> dropping table <br>";
-		executePlainSQL("Drop table animal");
-
-		echo "<br> creating new table <br>";
-		executePlainSQL("create table animal (
-		Animal_ID CHAR(6), 
-		Organization_ID CHAR(6),
-		Name VARCHAR(20),
-		Age INTEGER,
-		Gender CHAR(1), 
-		Breed VARCHAR(20),
-		Personality VARCHAR(20), 
-		Health_Considerations VARCHAR(20), 
-		Intake_Date DATE,
-		Price REAL)");
-		OCICommit($db_conn);
-		
-		echo "<br> dropping table <br>";
-		executePlainSQL("Drop table Animal_Shortlist");
-
-		echo "<br> creating new table <br>";
-		executePlainSQL("create table Animal_Shortlist (
-		User_ID CHAR(6),
-		Animal_ID CHAR(6), 
-		Organization_ID CHAR(6)
-		)");
-		OCICommit($db_conn);
-
-	} else
-		if (array_key_exists('ShortlistAdd', $_POST)) {
+	if (array_key_exists('ShortlistAdd', $_POST)) {
 		$tuple = array (
-			":bind1" => $_POST['Animal_ID'],
-			":bind2" => $_POST['Organization_ID']
+			":bind1" => $_POST['userid'],
+			":bind2" => $_POST['Animal_ID'],
+			":bind3" => $_POST['Organization_ID']
 		);
 		$alltuples = array (
 			$tuple
 		);
-		executeBoundSQL("insert into Animal_Shortlist values (NULL,:bind1, :bind2)", $alltuples);
+		executeBoundSQL("insert into Animal_Shortlist values (:bind1, :bind2, :bind3)", $alltuples);
 		OCICommit($db_conn);
 
 		} else
 			if (array_key_exists('ShortlistDelete', $_POST)) {
 				$tuple = array (
-					":bind1" => $_POST['Animal_ID'],
-					":bind2" => $_POST['Organization_ID']
+					":bind1" => $_POST['userid'],
+					":bind2" => $_POST['Animal_ID'],
+					":bind3" => $_POST['Organization_ID']
 				);
 				$alltuples = array (
 					$tuple
 				);
-				executeBoundSQL("delete from Animal_Shortlist where Animal_ID==:bind1 AND Organization_ID==:bind2 ", $alltuples);
+				executeBoundSQL("delete from Animal_Shortlist where User_ID=:bind1 AND Animal_ID=:bind2 AND Organization_ID=:bind3", $alltuples);
 				OCICommit($db_conn);
 	OCICommit($db_conn);
 	}
@@ -141,7 +109,6 @@ function run_Animaltable(){
            $columnNames = array("animal id", "Org ID", "name", "Age", "Gender", "Breed", "Personality", "Health", "Since", "Price");
            printTable($result, $columnNames);
 	}
-	OCICommit($db_conn);
 }
 
 function run_Shortlist(){
@@ -152,6 +119,5 @@ function run_Shortlist(){
            $columnNames = array("UserID(YOU)", "animal id", "Org ID");
            printTable($result, $columnNames);
 	}
-	OCICommit($db_conn);
 }
 ?>
