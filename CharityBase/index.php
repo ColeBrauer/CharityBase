b06860009ed1077d9bd024aa2c7b5ad227e9d94e
Margaret <m_yao11@hotmail.com>
2019-06-19 11:26:53 -0700

    converting to mysql for index.php

--- CharityBase/index.php
+++ CharityBase/index.php
@@ -22,7 +22,50 @@
 			<input type="submit" value="Login as Regular User" name="login_regular"/><br />
 			<input type="submit" value="Login as Admin" name="login_admin"/><br />
 		</form>
-		
+		<?php 
+		include 'db.php';
+		if (isset($_POST['Create_Regular_User'])) {
+			$userID = $_POST['userId'];
+			$userName = $_POST['name'];
+			$initialDonation = 0;
+
+			$sql1 = "INSERT INTO Person(User_ID, Name)
+					VALUES ('$userID', '$userName')";
+
+			$sql2 = "INSERT INTO Regular_User(User_ID, Donation_Total)
+					VALUES ('$userID', '$initialDonation')";
+
+			if (mysqli_query($con,$sql1)) {
+				echo "New Person created successfully";
+			} else { echo "Error: " . $sql1 . "<br>" . mysqli_error($con);}
+
+			if (mysqli_query($con,$sql2)) {
+				echo "New Regular User created successfully";
+			} else { echo "Error: " . $sql2 . "<br>" . mysqli_error($con);}
+			
+		} else {
+			
+			$userID = $_POST['userId'];
+			$userName = $_POST['name'];
+			$sql = "SELECT * FROM Person WHERE user_Id='$userID' AND name='$userName'";
+/*
+			if (isset($_POST['login_regular'])) {
+				if (mysqli_query($con, $sql)) {
+					header("Location: NormalHome.html");
+				} else {
+					echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+				}
+			}
+
+			if (isset($_POST['login_admin'])) {\
+				if (mysqli_query($con, $sql)) {
+					header("Location: AdminHome.html");
+				} else {
+					echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+				}
+			}*/
+		}
+		?>
 	</div>
 
 </body>
@@ -30,7 +73,6 @@
 
 
 <?php
-
 include 'db.php';
 if ($db_conn) {
 	if (array_key_exists('Create_Regular_User', $_POST)) {
